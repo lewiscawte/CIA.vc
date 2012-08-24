8ac954f2eb7b9082417e1cb4384cbe18acb09e48
lewiscawte@gmail.com <lewiscawte@gmail.com@85f3188c-4e3b-0410-a106-67708380cd12>
2012-08-24 10:59:43 +0000

    Remove (pointless) personal branch

--- branches/lcawteRewrite/cia/media-src/js/local/html.js
+++
@@ -1,8 +0,0 @@
-/* -*- Mode: C; c-basic-offset: 4 -*- 
- * Copyright (c) 2007 Micah Dowty <micah@navi.cx>
- */
-
-var htmlEscape = function(str)
-{
-    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;');
-}
--- branches/lcawteRewrite/cia/tools/http_logfilter.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-#
-# Extract HTTP logs from CIA's twistd.log in Combined Log Format
-#
-
-while (<>) {
-    print "$2\n" if /^\d{4}\/\d\d\/\d\d \d\d:\d\d [A-Z]+ \[(\-|HTTPChannel[^ \]]+)\] (\d+\.\d+\.\d+\.\d+ .*)/;
-}
-
